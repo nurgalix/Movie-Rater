9ce7bbcd3e267d9322637e35d04d6ded50afce7b
Nurgali Nuraly <nurali.nurgali@gmail.com>
2020-01-11 21:16:25 +0600

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { AuthModule } from './auth/auth.module';
+import { MainModule } from './main/main.module';
 
 import { AppComponent } from './app.component';
+
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'movies'}
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AuthModule,
+    MainModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
